28952d92fd3b8d986d7cd4b243514f7ee8c5f422
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-09 01:36:38 +0100

    feat: methods for consume api created

--- app/Http/Controllers/ConsumeController.php
+++ app/Http/Controllers/ConsumeController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ConsumeController extends Controller
 {
-    //
+
+    public $tokken;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function getData(){
+
+    }
+
+    public function view(){
+
+    }
+
+    public function create(){
+        
+    }
 }
