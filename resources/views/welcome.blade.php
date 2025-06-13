eee7209fb4479160d5264035b0ab0f4547a15d09
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-14 00:48:15 +0100

    style: imported file bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="{{ asset('assets/bootstrap-5.0.2/css/bootstrap.css') }}">
         <title>Consumo de API</title>
     </head>
     <body>
        <h1>View</h1>
+       <script src="{{ asset('assets/bootstrap-5.0.2/js/bootstrap.js') }}"></script>
     </body>
 </html>
