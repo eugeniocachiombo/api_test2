b12c838702eb477691c9d492a25ebdab57ae6211
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-13 23:27:21 +0100

    feat: set my token

--- app/Http/Controllers/ConsumeController.php
+++ app/Http/Controllers/ConsumeController.php
@@ -7,11 +7,11 @@ use Illuminate\Http\Request;
 class ConsumeController extends Controller
 {
 
-    public $tokken;
+    public $token;
 
     public function __construct()
     {
-        
+        $this->token = "live_YfB9dUuYxzclJOKwj7dTN8iBKUqKuGDWm7tLNUbi8MlrzhT1566djcMdMRaw4cXm";
     }
 
     public function getData(){
