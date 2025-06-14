6570e041bc5fbf4048da8e709d19c27e911aa130
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-14 17:17:41 +0100

    improving: route getData delete and view change

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\ConsumeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ConsumeController::class, "getData"]);
-Route::get('/home', [ConsumeController::class, "view"]);
+Route::get('/', [ConsumeController::class, "view"]);
